ab332733ca247d6b1f3cf64a9c32fe6aebd04f7b
rafitj <rafit.jamil@gmail.com>
2020-08-18 17:42:48 -0400

    fix directory

---
+++ src/Level.java
@@ -0,0 +1,164 @@
+import javafx.animation.AnimationTimer;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+enum GameLevel{
+    ONE,
+    TWO,
+    THREE,
+    FOUR
+}
+
+public class Level {
+    private ArrayList<Enemy> enemies;
+    public  Player player;
+
+    public int score;
+    public int level;
+    private GameLevel gameLevel;
+    private Text enemiesLeftText;
+    private MiniGame game;
+
+    private HashMap<GameLevel, ArrayList<Image>> enemy_map;
+    private HashMap<Image, Pair<Integer, Integer>> sprite_offsets;
+
+    private static Image loadPokemonImg(String pokemon, double size){
+        return new Image("assets/pokemon/"+pokemon+".gif",size,size,true,true);
+    }
+
+
+    private final static ArrayList<Image> LEVEL_1_ENEMIES = new ArrayList<Image>(List.of(loadPokemonImg("Geodude", 250),
+                                                         loadPokemonImg("Grimer", 210),loadPokemonImg("Magnemite", 190)));
+
+    private final static ArrayList<Image> LEVEL_2_ENEMIES = new ArrayList<Image>(List.of(loadPokemonImg("Crobat", 425),
+                                                            loadPokemonImg("Forretress", 235),loadPokemonImg("Skarmory", 375)));
+
+    private final static ArrayList<Image> LEVEL_3_ENEMIES = new ArrayList<Image>(List.of(loadPokemonImg("Gardevoir", 325),
+                                                        loadPokemonImg("Salamence", 400),loadPokemonImg("Ludicolo", 400)));
+
+    Level(){
+        enemies = new ArrayList<>();
+        enemy_map = new HashMap<>();
+        enemy_map.put(GameLevel.ONE,LEVEL_1_ENEMIES);
+        enemy_map.put(GameLevel.TWO,LEVEL_2_ENEMIES);
+        enemy_map.put(GameLevel.THREE,LEVEL_3_ENEMIES);
+        sprite_offsets = new HashMap<>();
+        sprite_offsets.put(LEVEL_1_ENEMIES.get(0),new Pair<>(0,0));
+        sprite_offsets.put(LEVEL_1_ENEMIES.get(1),new Pair<>(0,0));
+        sprite_offsets.put(LEVEL_1_ENEMIES.get(2),new Pair<>(0,0));
+        sprite_offsets.put(LEVEL_2_ENEMIES.get(0),new Pair<>(0,-110));
+        sprite_offsets.put(LEVEL_2_ENEMIES.get(1),new Pair<>(0,-25));
+        sprite_offsets.put(LEVEL_2_ENEMIES.get(2),new Pair<>(0,-95));
+        sprite_offsets.put(LEVEL_3_ENEMIES.get(0),new Pair<>(0,-120));
+        sprite_offsets.put(LEVEL_3_ENEMIES.get(1),new Pair<>(0,-140));
+        sprite_offsets.put(LEVEL_3_ENEMIES.get(2),new Pair<>(0,-135));
+        level = 1;
+        score = 0;
+    }
+
+
+    public void updateEnemyCount(Enemy e) {
+       enemies.remove(e);
+       enemiesLeftText.setText(enemies.size() + " Pokemon Left");
+       if (enemies.size() == 0) {
+           if(level == 3) {
+               game.setGamestate(GameState.GAME_WIN);
+           } else {
+               game.setGamestate(GameState.LEVEL_COMPLETE);
+
+           }
+          game.updateStage();
+       }
+    }
+
+    public Group getLevelEnemiesSprites() {
+        Group container = new Group();
+        ArrayList<Image> lvlEnemies = enemy_map.get(gameLevel);
+        int i = 0;
+        for (Image enemyImage : lvlEnemies) {
+            ImageView iv = new ImageView(enemyImage);
+            iv.setTranslateX(i*225 + 450);
+            i+=1;
+            container.getChildren().add(iv);
+        }
+        container.setLayoutX(-175);
+        container.setLayoutY(170 - (50*(gameLevel.ordinal()-1)));
+        return container;
+    }
+
+    public Group getEnemiesLeft(){
+        Group g = new Group();
+        enemiesLeftText = new Text(enemies.size() + " Pokemon Left");
+        enemiesLeftText.setFill(Color.BLACK);
+        enemiesLeftText.setOpacity(0.9);
+        enemiesLeftText.setStyle("-fx-font: 30 arial; -fx-font-weight: bold;");
+        enemiesLeftText.setLayoutX(1000);
+        enemiesLeftText.setLayoutY(670);
+        g.getChildren().add(enemiesLeftText);
+        return  g;
+    }
+
+    public void newLevel(MiniGame miniGame,GameLevel gLvl, Group parentGroup) {
+        gameLevel = gLvl;
+        game = miniGame;
+        level = gLvl.ordinal()+1;
+        player = new Player(miniGame, level);
+        Random rand = new Random();
+        ArrayList<Image> lvlEnemies = enemy_map.get(gameLevel);
+        enemies.clear();
+        for (int i = 0; i < level*10; i++) {
+            int randIndx = rand.nextInt(lvlEnemies.size());
+            Image enemy_img = lvlEnemies.get(randIndx);
+            Pair<Integer,Integer> offsets = sprite_offsets.get(enemy_img);
+            enemies.add(new Enemy(enemy_img, this, player, parentGroup, offsets.getKey(), offsets.getValue() ));
+        }
+        player.setEnemies(enemies);
+    }
+
+
+    public void spawnEnemies(Group g) {
+        int numEnemies = enemies.size();
+        ArrayList<Enemy> enemiesToSpawn = (ArrayList<Enemy>)enemies.clone();
+        Random rand = new Random();
+        AnimationTimer timer = new AnimationTimer() {
+            private int i = 0;
+            private long lastSpawn = 0 ;
+            @Override
+            public void handle(long now) {
+                if (i == numEnemies){
+                    this.stop();
+                } else if (now/1000000000 - lastSpawn/1000000000 >= (rand.nextDouble()*0.5)+(6-(level*2))) {
+                    enemiesToSpawn.get(i).spawn(rand.nextBoolean(), g);
+                    i+=1;
+                    lastSpawn = now;
+                }
+            }
+        };
+        timer.start();
+    }
+
+
+    public String getLevelInfo() {
+        String region;
+        switch (level) {
+            case 1:
+                region = "Kanto";
+                break;
+            case 2:
+                region = "Jhoto";
+                break;
+            default:
+                region = "Hoenn";
+        }
+        return region + " Region";
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,458 @@
+import javafx.animation.AnimationTimer;
+import javafx.scene.Group;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Text;
+import javafx.util.Pair;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Player {
+    private ArrayList<Enemy> enemies;
+    private int lives = 3;
+    private int score = 0;
+    private int evolutionStage;
+    public Sprite sprite;
+    private Rectangle healthbar = new Rectangle(600, 20, Color.LIGHTGREEN);
+    private Group hpgroup = new Group();
+    private Text hptext = new Text("3 / 3 HP");
+    private Rectangle scorebar = new Rectangle(0, 10, Color.LIGHTBLUE);
+    private Group scoregroup = new Group();
+    private Text scoretext = new Text("0 EXP");
+    private MiniGame game;
+    private ArrayList<String> evolutions =  new ArrayList<>(List.of("Charmander", "Charmeleon", "Charizard", "MegaCharizard"));
+    private Image fireImg;
+    private static final Media fireSound = new Media(new File("src/assets/audio/Fire.wav").toURI().toString());
+    private static final Media hitSound = new Media(new File("src/assets/audio/Hit.wav").toURI().toString());
+    private static Image fireDeath = new Image("assets/fire/burn.gif", 300, 300, true,true);
+    private static Image Charmander =  new Image("assets/pokemon/Charmander.gif",200,200,true,true);
+    private static Image Charmeleon =  new Image("assets/pokemon/Charmeleon.gif",250,250,true,true);
+    private static Image Charizard =  new Image("assets/pokemon/Charizard.gif",500,500,true,true);
+    private static Image MegaCharizard =  new Image("assets/pokemon/MegaCharizard.gif",600,600,true,true);
+    private ArrayList<Pair<Double,Double>> evolutionOffsets;
+
+    public void loseLife(){
+        MediaPlayer hitSFX = new MediaPlayer(hitSound);
+        hitSFX.stop();
+        hitSFX.play();
+        lives -= 1;
+        animateHealthBar();
+        if (lives == 1) {
+            healthbar.setFill(Color.RED);
+        } else if (lives == 2) {
+            healthbar.setFill(Color.ORANGE);
+        }
+        if (lives == 0) {
+            for (Enemy e : enemies) {
+                e.end();
+            }
+            resetPlayer();
+            game.setGamestate(GameState.GAME_OVER);
+            game.updateStage();
+        }
+    }
+
+
+
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(int s) {
+        if (s == 0 || score == 0){
+            score = s;
+        } else if (score < 0) {
+            score = s+1;
+        } else if (score > 0) {
+            score = s-1;
+        }
+        scoretext.setText(score +" XP");
+        scorebar.setWidth(score);
+    }
+
+    public void gainXP() {
+        score += 1;
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (scorebar.getWidth() > score*10) {
+                    this.stop();
+                }
+                scorebar.setWidth(scorebar.getWidth()+1);
+            }
+        };
+        timer.start();
+        scoretext.setText(score + " XP");
+    }
+
+    public void loseXP() {
+        score -= 1;
+        scoretext.setText(score + " XP");
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (scorebar.getWidth() < score*10) {
+                    this.stop();
+                }
+                scorebar.setWidth(scorebar.getWidth()-1);
+            }
+        };
+        timer.start();
+        scoretext.setText(score + " XP");
+    }
+
+    private void animateHealthBar() {
+        double endWidth = healthbar.getWidth()-200;
+        AnimationTimer timer = new AnimationTimer() {
+            int i = 5;
+            @Override
+            public void handle(long now) {
+                if (healthbar.getWidth() < endWidth) {
+                    this.stop();
+                } else {
+                    healthbar.setWidth(healthbar.getWidth()-5);
+                    if (i <= 100) {
+                        i+=5;
+                    }
+                }
+            }
+        };
+        hptext.setText(lives+" / 3 HP");
+        timer.start();
+    }
+
+    public void resetPlayer() {
+        lives = 3;
+    }
+
+    Player(MiniGame mg, int stage) {
+        evolutionOffsets = new ArrayList<>();
+        evolutionOffsets.add(new Pair<Double, Double>(0.0,0.0));
+        evolutionOffsets.add(new Pair<Double, Double>(-25.0,-25.0));
+        evolutionOffsets.add(new Pair<Double, Double>(-125.0,-200.0));
+        evolutionOffsets.add(new Pair<Double, Double>(-200.0,-120.0));
+
+        game = mg;
+        evolutionStage = stage;
+        fireImg = new Image("assets/fire/fire"+evolutionStage+".gif", 100*evolutionStage, 100*evolutionStage, true, true);
+        Image sprtieImg;
+        switch (evolutionStage){
+            case 1:
+                sprtieImg=Charmander;
+                break;
+            case 2:
+                sprtieImg=Charmeleon;
+                break;
+            default:
+                sprtieImg=Charizard;
+                break;
+        }
+        sprite = new Sprite(sprtieImg);
+        if (evolutionStage == 2) {
+            sprite.spriteView.setLayoutY(sprite.spriteView.getLayoutY()-50);
+            sprite.spriteView.setLayoutX(sprite.spriteView.getLayoutX());
+        }
+        else if (evolutionStage == 3) {
+            sprite.spriteView.setLayoutY(sprite.spriteView.getLayoutY()-250);
+            sprite.spriteView.setLayoutX(sprite.spriteView.getLayoutX());
+        }
+        else if (evolutionStage == 4) {
+            sprite.spriteView.setLayoutY(sprite.spriteView.getLayoutY()-170);
+            sprite.spriteView.setLayoutX(sprite.spriteView.getLayoutX()-50);
+        }
+    }
+
+    public void setEnemies(ArrayList<Enemy> e) {
+        enemies = e;
+    }
+
+    public void fireLeft(Group parentGroup) {
+        sprite.spriteView.setScaleX(1);
+        MediaPlayer fireSFX = new MediaPlayer(fireSound);
+        fireSFX.stop();
+        fireSFX.play();
+        ImageView fireView = createFireballView(fireImg,90, (640-15-fireImg.getWidth()), 405-(evolutionStage*72));
+        Group fireGroup = new Group();
+        fireGroup.getChildren().add(fireView);
+        parentGroup.getChildren().add(fireGroup);
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (fireView.getOpacity() < 0.05) {
+                    animateMissedFire(parentGroup, 545+fireView.getTranslateX(), fireView.getLayoutY());
+                    parentGroup.getChildren().remove(fireGroup);
+                    loseXP();
+                    this.stop();
+                } else {
+                    if (fireView.getTranslateX() < -360+((evolutionStage-1)*120)) {
+                        fireView.setOpacity(fireView.getOpacity()-(0.1*evolutionStage));
+                    }
+                    fireView.setTranslateX(fireView.getTranslateX()-10);
+                    // Check if fireball hits any enemies
+                    for (Enemy e : enemies) {
+                        if (!e.spawned) {
+                            continue;
+                        }
+                        if (fireGroup.intersects(e.sprite.spriteGroup.getBoundsInParent())){
+                            enemyBurn(parentGroup,(0-e.sprite.spriteView.getImage().getWidth())+e.sprite.spriteView.getTranslateX(), e.sprite.spriteView.getLayoutY(), true);
+                            parentGroup.getChildren().remove(fireGroup);
+                            fireGroup.getChildren().clear();
+                            gainXP();
+                            e.defeated();
+                            this.stop();
+                            break;
+                        }
+                    }
+                }
+            }
+        };
+        timer.start();
+
+    }
+
+    public void fireRight(Group parentGroup) {
+        sprite.spriteView.setScaleX(-1);
+        MediaPlayer fireSFX = new MediaPlayer(fireSound);
+        fireSFX.stop();
+        fireSFX.play();
+        ImageView fireView = createFireballView(fireImg, -90, 655, 405-(evolutionStage*72));
+        Group fireGroup = new Group();
+        fireGroup.getChildren().add(fireView);
+
+        parentGroup.getChildren().add(fireGroup);
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (fireView.getOpacity() < 0.05) {
+                    parentGroup.getChildren().remove(fireGroup);
+                    animateMissedFire(parentGroup, 657+fireView.getTranslateX(), fireView.getLayoutY());
+                    loseXP();
+                    this.stop();
+                }
+                if (fireView.getTranslateX() > 360-((evolutionStage-1)*120)) {
+                    fireView.setOpacity(fireView.getOpacity()-(0.1*evolutionStage));
+                }
+                fireView.setTranslateX(fireView.getTranslateX()+10);
+                // Check if fireball hits any enemies
+                // Check if fireball hits any enemies
+                for (Enemy e : enemies) {
+                    if (!e.spawned) {
+                        continue;
+                    }
+                    if (fireGroup.intersects(e.sprite.spriteGroup.getBoundsInParent())){
+                        enemyBurn(parentGroup,1280+e.sprite.spriteView.getTranslateX(), e.sprite.spriteView.getLayoutY(), false);
+                        parentGroup.getChildren().remove(fireGroup);
+                        fireGroup.getChildren().clear();
+                        gainXP();
+                        e.defeated();
+                        this.stop();
+                        break;
+                    }
+                }
+            }
+        };
+        timer.start();
+    }
+
+    private void animateMissedFire(Group parentGroup, double x, double y){
+        Text pointText = new Text("-1XP");
+        pointText.setFill(Color.GREY);
+        pointText.setStroke(Color.BLACK);
+        pointText.setStrokeWidth(2);
+        pointText.setStrokeType(StrokeType.OUTSIDE);
+        pointText.setStyle("-fx-font: 40 arial; -fx-font-weight: bold;");
+        pointText.setLayoutX(x);
+        pointText.setLayoutY(y);
+        pointText.setOpacity(0);
+        parentGroup.getChildren().addAll(pointText);
+        AnimationTimer timer = new AnimationTimer() {
+            int i = 80;
+            @Override
+            public void handle(long l) {
+                if (i < 1){
+                    parentGroup.getChildren().remove(pointText);
+                    this.stop();
+                } else {
+                    if (i > 60) {
+                        pointText.setOpacity(pointText.getOpacity()+0.10);
+                    } else {
+                        pointText.setOpacity(pointText.getOpacity()-0.10);
+                    }
+                    pointText.setTranslateY(pointText.getTranslateY()-1);
+                    i -= 1;
+                }
+            }
+        };
+
+        timer.start();
+    }
+
+    private void enemyBurn(Group parentGroup, double x, double y, boolean isLeft) {
+        ImageView fire = new ImageView(fireDeath);
+        fire.setOpacity(0);
+        fire.setLayoutX(x);
+        fire.setLayoutY(y);
+        Text pointText = new Text("+1XP");
+        pointText.setFill(Color.LIGHTBLUE);
+        pointText.setStroke(Color.BLACK);
+        pointText.setStrokeWidth(2);
+        pointText.setStrokeType(StrokeType.OUTSIDE);
+        pointText.setStyle("-fx-font: 40 arial; -fx-font-weight: bold;");
+        double lx = x;
+        if (x < 50 && isLeft) {
+            lx += 160;
+        } else if(isLeft) {
+            lx -= 40;
+        } else if (x > 1250){
+            lx -= 70;
+        }
+        pointText.setLayoutX(lx);
+        pointText.setLayoutY(y+50);
+        pointText.setOpacity(0);
+        parentGroup.getChildren().addAll(fire, pointText);
+        AnimationTimer timer = new AnimationTimer() {
+            int i = 60;
+            @Override
+            public void handle(long l) {
+                if (i < 1){
+                    fire.setOpacity(0);
+                    parentGroup.getChildren().remove(pointText);
+                    parentGroup.getChildren().remove(fire);
+                    this.stop();
+                } else {
+                    if (i > 50) {
+                        fire.setOpacity(fire.getOpacity()+0.20);
+                        pointText.setOpacity(pointText.getOpacity()+0.20);
+                    } else {
+                        fire.setOpacity(fire.getOpacity()-0.10);
+                        pointText.setOpacity(pointText.getOpacity()-0.10);
+                    }
+                    pointText.setTranslateY(pointText.getTranslateY()-1);
+                    i -= 1;
+                }
+            }
+        };
+
+        timer.start();
+    }
+    private ImageView createFireballView(Image fire, int rotation, double startX, double startY) {
+        ImageView fireView = new ImageView(fire);
+        fireView.setRotate(rotation);
+        fireView.setLayoutX(startX);
+        fireView.setLayoutY(startY);
+        return fireView;
+    }
+
+    public Group getPlayerHPGroup(){
+        hptext.setX(270);
+        hptext.setY(45);
+        hptext.setFill(Color.BLACK);
+        hptext.setOpacity(0.7);
+        hptext.setStyle("-fx-font: 20 arial; -fx-font-weight: bold;");
+        healthbar.setArcWidth(20.0);
+        healthbar.setArcHeight(20.0);
+        healthbar.setStrokeWidth(4);
+        healthbar.setStroke(Color.BLACK);
+        Rectangle healthbarBackground = new Rectangle(600, 20, Color.BLACK);
+        healthbarBackground.setArcWidth(20.0);
+        healthbarBackground.setArcHeight(20.0);
+        healthbarBackground.setStrokeWidth(4);
+        healthbarBackground.setStroke(Color.BLACK);
+        hpgroup.getChildren().addAll(healthbarBackground, healthbar, hptext);
+        hpgroup.setLayoutX(340);
+        hpgroup.setLayoutY(600);
+        return hpgroup;
+    }
+    public Group getScoregroup(){
+        scoretext.setX(275);
+        scoretext.setY(35);
+        scoretext.setFill(Color.BLACK);
+        scoretext.setOpacity(0.7);
+        scoretext.setStyle("-fx-font: 20 arial; -fx-font-weight: bold;");
+        scorebar.setArcWidth(10.0);
+        scorebar.setArcHeight(10.0);
+        Rectangle scorebarBG = new Rectangle(600, 10, Color.WHITE);
+        scorebarBG.setArcWidth(10.0);
+        scorebarBG.setArcHeight(10.0);
+        scorebarBG.setStrokeWidth(4);
+        scorebarBG.setStroke(Color.WHITE);
+        scoregroup.getChildren().addAll(scorebarBG, scorebar, scoretext);
+        scoregroup.setLayoutX(340);
+        scoregroup.setLayoutY(660);
+        return scoregroup;
+    }
+
+    public  ImageView evolve(Text evolveText){
+        String pokemon = evolutions.get(evolutionStage-1);
+        MediaPlayer sound = new MediaPlayer(new Media(new File("src/assets/audio/"+pokemon+".wav").toURI().toString()));
+        sound.play();
+        evolveText.setText("What? " + evolutions.get(evolutionStage-1) + " is evolving!");
+        ImageView evolveView;
+       if (evolutionStage == 1) {
+           evolveView = new ImageView(Charmander);
+           toggle(evolveView,Charmander, Charmeleon,evolveText);
+       } else if (evolutionStage == 2){
+           evolveView = new ImageView(Charmeleon);
+           toggle(evolveView,Charmeleon, Charizard,evolveText);
+       } else {
+           evolveView = new ImageView(Charizard);
+           toggle(evolveView,Charizard, MegaCharizard,evolveText);
+       }
+       evolveView.setLayoutX(565);
+       evolveView.setLayoutY(260);
+       evolveView.setTranslateX(evolutionOffsets.get(evolutionStage-1).getKey());
+       evolveView.setTranslateY(evolutionOffsets.get(evolutionStage-1).getValue());
+       return evolveView;
+    }
+
+    private void toggle(ImageView iv, Image a, Image b, Text evolveText) {
+        ColorAdjust whiteout = new ColorAdjust();
+        whiteout.setBrightness(1.0);
+        AnimationTimer timer = new AnimationTimer() {
+            private int i = 1;
+            private long lastUpdate = 0;
+            @Override
+            public void handle(long now) {
+                if (600000000-(i*10000000) < 1500000) {
+                    iv.setEffect(null);
+                    iv.setImage(b);
+                    String pokemon = evolutions.get(evolutionStage);
+                    MediaPlayer evolvedSound = new MediaPlayer(new Media(new File("src/assets/audio/"+pokemon+".wav").toURI().toString()));
+                    evolvedSound.play();
+                    evolveText.setText("Congrats! " + evolutions.get(evolutionStage-1) + " evolved into " + pokemon + "!");
+                    evolveText.setLayoutX(220);
+                    if (evolutionStage != 3) {
+                        evolutionStage += 1;
+                    }
+                    fireImg = new Image("assets/fire/fire"+evolutionStage+".gif", 200, 200, true, true);
+                    this.stop();
+                } else if (now-lastUpdate > 400000000-(i*25000000)) {
+                    lastUpdate = now;
+                    if (i < 3) {}
+                    else if (i%3!=0) {
+                        iv.setImage(b);
+                        iv.setTranslateX(evolutionOffsets.get(evolutionStage).getKey());
+                        iv.setTranslateY(evolutionOffsets.get(evolutionStage).getValue());
+                    } else {
+                        iv.setEffect(whiteout);
+                        iv.setImage(a);
+                        iv.setTranslateX(evolutionOffsets.get(evolutionStage-1).getKey());
+                        iv.setTranslateY(evolutionOffsets.get(evolutionStage-1).getValue());
+                    }
+                    i++;
+                }
+            }
+        };
+        timer.start();
+    }
+}
